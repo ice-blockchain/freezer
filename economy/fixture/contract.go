9bf977a616d45dcf46b2f63092c0e7b9b1d9913d
Alexey Zaiats <azaiats@infoblox.com>
2022-04-28 14:36:39 +0300

    - fix conflict

--- economy/fixture/contract.go
+++ economy/fixture/contract.go
@@ -2,14 +2,6 @@
 
 package fixture
 
-<<<<<<< HEAD
 import "errors"
 
 var errFixtureCleanupFailed = errors.New("economy fixture cleanup failed")
-=======
-import (
-	"errors"
-)
-
-var errFixtureCleanup = errors.New("economy fixture cleanup failed")
->>>>>>> Start Mining endpoint implementation. Makefile coverage was chang…
