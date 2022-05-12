44166c91cc406948d2608c150c1d1e93b972e740
Alexey Zaiats <azaiats@infoblox.com>
2022-05-12 12:32:51 +0300

    - get top miners implementation - fix receiver name

--- economy/contract.go
+++ economy/contract.go
@@ -49,7 +49,6 @@ type (
 	}
 	TopMiner struct {
 		UserID            string  `json:"userId" example:"did:ethr:0x4B73C58370AEfcEf86A6021afCDe5673511376B2"`
-		Username          string  `json:"username" example:"jdoe"`
 		ProfilePictureURL string  `json:"profilePictureURL" example:"https://somecdn.com/p1.jpg"`
 		Balance           float64 `json:"balance" example:"232.5"`
 	}
@@ -65,6 +64,7 @@ type (
 	// ReadRepository manages the database operations related to `users_economy`.
 	ReadRepository interface {
 		GetUserEconomy(context.Context, string, bool) (*UserEconomy, error)
+		GetTopMiners(context.Context, uint64, uint64) ([]*TopMiner, error)
 	}
 	// WriteRepository manage the database operations related to `user_economy`.
 	WriteRepository interface {
@@ -122,6 +122,15 @@ type (
 	// | miningStarted is internal structure to hold notification message.
 	miningStarted struct {
 		TS time.Time `json:"ts"`
+	}
+
+	// | topMiner is internal structure for deserialization from the DB.
+	topMiner struct {
+		//nolint:unused // Because it is used by the msgpack library for marshalling/unmarshalling.
+		_msgpack          struct{} `msgpack:",asArray"`
+		UserID            UserID
+		ProfilePictureURL string
+		Balance           float64
 	}
 
 	// | repository implements the public API that this package exposes.
---
+++ economy/get_top_miners.go
@@ -0,0 +1,44 @@
+package economy
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+func (e *economy) GetTopMiners(ctx context.Context, limit, offset uint64) ([]*TopMiner, error) {
+	if ctx.Err() != nil {
+		return nil, errors.Wrap(ctx.Err(), "get top miners failed because context failed")
+	}
+
+	space := userEconomySpace()
+	params := map[string]interface{}{
+		"limit":  limit,
+		"offset": offset,
+	}
+
+	sql := fmt.Sprintf(`SELECT user_id, profile_picture_url, balance 
+		FROM %[1]v INDEXED BY "pk_unnamed_%[1]v_1" 
+		ORDER BY balance LIMIT :limit OFFSET :offset`, space)
+
+	var res []*topMiner
+	if err := e.db.PrepareExecuteTyped(sql, params, &res); err != nil {
+		return nil, errors.Wrapf(err, "failed to get %q record with limit:%v and offset:%v", space, limit, offset)
+	}
+
+	topMiners := make([]*TopMiner, 0, len(res))
+	for _, item := range res {
+		topMiners = append(topMiners, item.toTopMiner())
+	}
+
+	return topMiners, nil
+}
+
+func (t *topMiner) toTopMiner() *TopMiner {
+	return &TopMiner{
+		UserID:            t.UserID,
+		ProfilePictureURL: t.ProfilePictureURL,
+		Balance:           t.Balance,
+	}
+}
