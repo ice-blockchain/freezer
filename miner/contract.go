09f516dd9ff491476ad2b4d351b1051e2ea28148
ice-myles <96409608+ice-myles@users.noreply.github.com>
2023-11-02 11:53:30 +0300

    Revert t1/t2 balance and t1/t2 active counts recalculation.
    This reverts commits 064227693f34a830d89e4c07b17f06d177b1e7cf (#33), 93040b88d4980529a18d319712d00a26d64e84ae (#35), 77b2f69249b804284fea7f800c8d334749f0f24d (#36), 3768872fbfb1feaca20c86b25f763350c04838de (#37), 227611e6cf4c1e1be77f791b1a83417d2d456d5d (#38).

--- miner/contract.go
+++ miner/contract.go
@@ -4,7 +4,6 @@ package miner
 
 import (
 	"context"
-	_ "embed"
 	"io"
 	"sync"
 	"sync/atomic"
@@ -14,7 +13,6 @@ import (
 	"github.com/ice-blockchain/freezer/model"
 	"github.com/ice-blockchain/freezer/tokenomics"
 	messagebroker "github.com/ice-blockchain/wintr/connectors/message_broker"
-	storagePG "github.com/ice-blockchain/wintr/connectors/storage/v2"
 	"github.com/ice-blockchain/wintr/connectors/storage/v3"
 	"github.com/ice-blockchain/wintr/time"
 )
@@ -49,9 +47,6 @@ const (
 var (
 	//nolint:gochecknoglobals // Singleton & global config mounted only during bootstrap.
 	cfg config
-
-	//go:embed .testdata/DDL.sql
-	eskimoDDL string
 )
 
 type (
@@ -63,7 +58,6 @@ type (
 		model.ExtraBonusStartedAtField
 		model.LatestDeviceField
 		model.UserIDField
-		model.UsernameField
 		UpdatedUser
 		model.BalanceSoloPendingField
 		model.BalanceT1PendingField
@@ -114,22 +108,6 @@ type (
 		model.IDTMinus1Field
 	}
 
-	backupUserUpdated struct {
-		model.BalancesBackupUsedAtField
-		model.UserIDField
-		model.BalanceT1Field
-		model.BalanceT2Field
-		model.SlashingRateT1Field
-		model.SlashingRateT2Field
-		model.FirstRecalculatedBalanceT1Field
-		model.FirstRecalculatedBalanceT2Field
-		model.DeserializedBackupUsersKey
-		model.ActiveT1ReferralsField
-		model.ActiveT2ReferralsField
-		model.FirstRecalculatedActiveT1ReferralsField
-		model.FirstRecalculatedActiveT2ReferralsField
-	}
-
 	referral struct {
 		model.MiningSessionSoloStartedAtField
 		model.MiningSessionSoloEndedAtField
@@ -160,14 +138,12 @@ type (
 	miner struct {
 		mb                            messagebroker.Client
 		db                            storage.DB
-		dbPG                          *storagePG.DB
 		dwhClient                     dwh.Client
 		cancel                        context.CancelFunc
 		telemetry                     *telemetry
 		wg                            *sync.WaitGroup
 		extraBonusStartDate           *time.Time
 		extraBonusIndicesDistribution map[uint16]map[uint16]uint16
-		recalculationBalanceStartDate *time.Time
 	}
 	config struct {
 		disableAdvancedTeam *atomic.Pointer[[]string]
