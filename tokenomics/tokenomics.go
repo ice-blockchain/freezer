cce2ca194641047849f2966b3369fa2f85a37ec9
Ares <75481906+ice-ares@users.noreply.github.com>
2023-12-01 09:48:01 +0200

    added extra logging for tokenomics processor

--- tokenomics/tokenomics.go
+++ tokenomics/tokenomics.go
@@ -75,6 +75,7 @@ func StartProcessor(ctx context.Context, cancel context.CancelFunc) Processor {
 	prc.extraBonusIndicesDistribution = extrabonusnotifier.MustGetExtraBonusIndicesDistribution(ctx, prc.db)
 	prc.livenessLoadDistributionStartDate = mustGetLivenessLoadDistributionStartDate(ctx, prc.db)
 	log.Info(fmt.Sprintf("configuration loaded[livenessLoadDistributionStartDate]: %#v", prc.livenessLoadDistributionStartDate))
+	log.Info(fmt.Sprintf("configuration loaded[FaceRecognitionDelay]: %#v", cfg.KYC.FaceRecognitionDelay))
 	log.Info(fmt.Sprintf("configuration loaded[LivenessDelay]: %#v", cfg.KYC.LivenessDelay))
 	log.Info(fmt.Sprintf("configuration loaded[AdoptionMilestoneSwitch]: %#v", cfg.AdoptionMilestoneSwitch))
 	log.Info(fmt.Sprintf("configuration loaded[ExtraBonuses]: %#v", cfg.ExtraBonuses))
