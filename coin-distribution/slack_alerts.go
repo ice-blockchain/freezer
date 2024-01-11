08c161652a838e235c7265bceb9a06ef9c1197de
ice-cronus <105345303+ice-cronus@users.noreply.github.com>
2024-01-11 14:53:32 +0300

    reformat messages

--- coin-distribution/slack_alerts.go
+++ coin-distribution/slack_alerts.go
@@ -17,13 +17,13 @@ import (
 )
 
 func (r *repository) sendCurrentCoinDistributionsAvailableForReviewAreApprovedSlackMessage(ctx context.Context, recipients uint64, iceCoins float64) error {
-	text := fmt.Sprintf(":white_check_mark:`%v` current pending coin distributions are approved (`%v` recipients, `%.2f` ice coins) and are going to be processed as soon as the coin-distributer comes online :white_check_mark:", r.cfg.Environment, recipients, iceCoins) //nolint:lll // .
+	text := fmt.Sprintf(":white_check_mark:`%v` current pending coin distributions are approved and are going to be processed as soon as the coin-distributer comes online :white_check_mark:\n`users`: `%v`\n`coins`: `%v`", r.cfg.Environment, recipients, fmt.Sprintf("%.2f", iceCoins)) //nolint:lll // .
 
 	return errors.Wrap(sendSlackMessage(ctx, text, r.cfg.AlertSlackWebhook), "failed to sendSlackMessage")
 }
 
 func (r *repository) sendCurrentCoinDistributionsAvailableForReviewAreApprovedToBeProcessedImmediatelySlackMessage(ctx context.Context, recipients uint64, iceCoins float64) error {
-	text := fmt.Sprintf(":white_check_mark::zap:`%v` current pending coin distributions are approved (`%v` recipients, `%.2f` ice coins) and are going to be processed immediately :zap::white_check_mark:", r.cfg.Environment, recipients, iceCoins) //nolint:lll // .
+	text := fmt.Sprintf(":white_check_mark::zap:`%v` current pending coin distributions are approved and are going to be processed immediately :zap::white_check_mark:\n`users`: `%v`\n`coins`: `%v`", r.cfg.Environment, recipients, fmt.Sprintf("%.2f", iceCoins)) //nolint:lll // .
 
 	return errors.Wrap(sendSlackMessage(ctx, text, r.cfg.AlertSlackWebhook), "failed to sendSlackMessage")
 }
